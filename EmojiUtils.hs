9146f8ee183fba6456782afce5b90899b35b3d3c
XIAZY <i@mechanus.io>
2020-04-16 04:29:14 -0400

    add getTexts

--- EmojiUtils.hs
+++ EmojiUtils.hs
@@ -6,5 +6,8 @@ import Data.Text.ICU
 getBreaks :: String -> [Break ()]
 getBreaks input = breaks (breakCharacter Current) (pack input)
 
+getTexts :: String -> [Text]
+getTexts input = Prelude.map brkBreak (getBreaks input)
+
 length :: String -> Int
 length input = Prelude.length (getBreaks input)
