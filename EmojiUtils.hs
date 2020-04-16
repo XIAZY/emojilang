bb7fc5199b6c5471c28b6ba3f79f2a97cde94b6f
XIAZY <i@mechanus.io>
2020-04-16 04:38:25 -0400

    add type and comments

--- EmojiUtils.hs
+++ EmojiUtils.hs
@@ -3,11 +3,17 @@ module EmojiUtils where
 import Data.Text
 import Data.Text.ICU
 
+-- emoji are quite complicated. they can take any number of unicode 
+-- codepoints. use icu to break them
+
+type Emoji = [Text]
+
 getBreaks :: String -> [Break ()]
 getBreaks input = breaks (breakCharacter Current) (pack input)
 
-getTexts :: String -> [Text]
-getTexts input = Prelude.map brkBreak (getBreaks input)
+-- get emoji from string
+getEmoji :: String -> Emoji
+getEmoji input = Prelude.map brkBreak (getBreaks input)
 
 length :: String -> Int
 length input = Prelude.length (getBreaks input)
