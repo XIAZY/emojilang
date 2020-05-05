53e8729bed6124835dfb66352fb4052acade2fed
XIAZY <i@mechanus.io>
2020-05-05 13:47:21 -0400

    add main

--- MainParser.hs
+++ MainParser.hs
@@ -1,8 +1,13 @@
-module MainParser where
+module Main where
 
+import Control.Monad
 import qualified StmtParser as SP
 import qualified StmtDef as SD
 import ParserLib
 
 mainParser :: Parser SD.Stmt
 mainParser = whitespaces *> SP.stmts <* eof
+
+main = do
+    input <- getContents 
+    putStr (show (runStringParser mainParser input))
