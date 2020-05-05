2b80cc09e61fd1340c1045a967446d54b18c0687
XIAZY <i@mechanus.io>
2020-05-05 15:44:08 -0400

    set default input to be utf8

--- MainParser.hs
+++ MainParser.hs
@@ -5,11 +5,14 @@ import qualified StmtParser                    as SP
 import qualified StmtDef                       as SD
 import           ParserLib
 import           System.Environment
+import           System.IO
 
 mainParser :: Parser SD.Stmt
 mainParser = whitespaces *> SP.stmts <* eof
 
 main = do
-    (filepath:_) <- getArgs
-    content <- readFile filepath
+    (filepath : _) <- getArgs
+    h              <- openFile filepath ReadMode
+    hSetEncoding h utf8
+    content <- hGetContents h
     putStr (show (runStringParser mainParser content))
