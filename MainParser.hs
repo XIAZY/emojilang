e53c787aabccf500a32a4316490b8929dd6b008e
XIAZY <i@mechanus.io>
2020-05-05 15:29:56 -0400

    now mainparser takes filepath as input

--- MainParser.hs
+++ MainParser.hs
@@ -1,13 +1,15 @@
 module Main where
 
-import Control.Monad
-import qualified StmtParser as SP
-import qualified StmtDef as SD
-import ParserLib
+import           Control.Monad
+import qualified StmtParser                    as SP
+import qualified StmtDef                       as SD
+import           ParserLib
+import           System.Environment
 
 mainParser :: Parser SD.Stmt
 mainParser = whitespaces *> SP.stmts <* eof
 
 main = do
-    input <- getContents 
-    putStr (show (runStringParser mainParser input))
+    (filepath:_) <- getArgs
+    content <- readFile filepath
+    putStr (show (runStringParser mainParser content))
